c73980bab0c848db860c52ca197fd27e0580b6f0
Colin K. <colin_keel@berkeley.edu>
2025-02-11 02:21:51 -0800

    Adding homework 2

---
+++ homework2/hw2.py
@@ -0,0 +1,29 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# cd ~/Desktop
+
+# 2) What does ~/ mean?
+# ~/ represents where all of your folders are saved (like a home page for all of your folders).
+
+# 3) What's the difference between and abolute and a relative path in your terminal?
+# An absolute path uses the full path to a file, a relative path uses the path to the file starting in the directory you're currently working in.
+
+# 4) What command would bring you back to your home directory?
+# cd ~/
+
+# 5) If you called rm ./ in your current working directory, what would it do?
+# It would result in an error.
+
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# git commit moves changes from your directory to the staging area, git commit saves changes in the staging area, git push puts whats in the staging area on GitHub.
+
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# "Your branch is up to date" this is saying that there's nothing new to add to the staging area. 
+
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# Having a duplicate of my colin_decal folder in my colin_decal folder caused some problems when trying to use git, I solved it by deleting the duplicated folder.
+
+# 9) How does cloning a repository differ from pulling from a repository?
+# Cloning downloads an entire repository, pulling from a repository updates your repository with changes that have been made on the remote repository.
+
+# 10) Tell me a fun fact!
+# The idea of a carbon footprint was fabricated by British Petroleum to push the burden of climate change off of corporations and onto the individual.
