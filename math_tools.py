9c39ccbfeac7518ae35097642b0b1e66cccb3ef1
Colin K. <colin_keel@berkeley.edu>
2025-04-08 15:49:17 -0700

    adding math_tools

---
+++ math_tools.py
@@ -0,0 +1,12 @@
+def add(a, b):      #adds two values (a and b)
+    return(a+b)
+
+def subtract(a, b):     #subtracts b from a
+    return(a-b)
+
+def multiply(a, b):     #multiplies a and b
+    return (a*b)
+
+def divide(a, b):       #divides a by b
+    return(a/b)
+
